8ef9896ad3feb1df0cf4d5b6531ae878e5b79dbb
RaqeebDev <raqeebauto@gmail.com>
2026-01-24 18:27:00 +0530

    routes skeleton

---
+++ user.js
@@ -0,0 +1,23 @@
+
+
+
+
+app.post("/user/signup" , (req,res) =>{
+    res.json({
+        message:"signup endpoint"
+    })
+})
+
+app.post("/user/signin" , (req,res) =>{
+res.json({
+        message:"signin endpoint"
+    })
+})
+
+app.get("/user/purchases" , (req,res) =>{
+
+})
+
+app.post("/user/purchase" , (req,res) =>{
+
+})
