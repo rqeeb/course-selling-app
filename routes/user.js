c3178fb027f948b85f74bed8af3cf1dc219f8eb0
RaqeebDev <raqeebauto@gmail.com>
2026-01-25 22:23:02 +0530

    routing logic for user-router:

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,27 @@
 const { Router } = require("express");
 const userRouter = Router();
+const userModel = require("../db");
 
-userRouter.post("/signup", (req, res) => {
-  res.json({
-    message: "signup endpoint",
-  });
+userRouter.post("/signup", async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+  //Hash Password here!
+
+  try {
+    await userModel.create({
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+    });
+
+    res.json({
+      message: "signup suceed",
+    });
+  } catch (e) {
+    res.status(500).json({
+      error: e.message || e,
+    });
+  }
 });
 
 userRouter.post("/signin", (req, res) => {
