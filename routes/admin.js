ecc97b8eeac6ae538b72f35c783355338ebb9a46
RaqeebDev <raqeebauto@gmail.com>
2026-01-28 17:57:08 +0530

    added auth enpt for admin & user

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,56 @@
 const { Router } = require("express");
 const adminRouter = Router();
 const { adminModel } = require("../db.js");
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
 
-adminRouter.post("/signup", (req, res) => {
-  res.json({
-    message: "signup endpoint",
-  });
+adminRouter.post("/signup", async (req, res) => {
+  const { email, password, firstName, lastName } = req.body;
+  //Hash Password here!: No need [Admin Panel]
+
+  try {
+    await adminModel.create({
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+    });
+
+    res.status(200).json({
+      message: "signup suceed",
+    });
+  } catch (e) {
+    res.status(500).json({
+      error: e.message || e,
+    });
+  }
 });
 
-adminRouter.post("/signin", (req, res) => {
-  res.json({
-    message: "signin endpoint",
+adminRouter.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  //Password in DB is hashed but user provided NOT HASHED version - so UNHASH & compare.
+
+  const adminExists = await adminModel.findOne({
+    email: email,
+    password: password,
   });
+
+  if (adminExists) {
+    const token = jwt.sign(
+      {
+        id: adminExists._id,
+      },
+      JWT_ADMIN_PASSWORD,
+    );
+    res.status(200).json({
+      token: token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Invalid credentials",
+    });
+  }
 });
 
 adminRouter.post("/course", (req, res) => {
@@ -26,8 +65,6 @@ adminRouter.put("/course", (req, res) => {
   });
 });
 
-
-
 module.exports = {
-    adminRouter: adminRouter,
-}
+  adminRouter: adminRouter,
+};
--- routes/user.js
+++ routes/user.js
@@ -6,7 +6,7 @@ const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
 
 userRouter.post("/signup", async (req, res) => {
   const { email, password, firstName, lastName } = req.body;
-  //Hash Password here!
+  //Hash Password here! - can use bcrypt/Hash256?
 
   try {
     await userModel.create({
@@ -26,10 +26,31 @@ userRouter.post("/signup", async (req, res) => {
   }
 });
 
-userRouter.post("/signin", (req, res) => {
-  res.json({
-    message: "signin endpoint",
+userRouter.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+
+  //Password in DB is hashed but user provided NOT HASHED version - so UNHASH & compare.
+
+  const userExists = await userModel.findOne({
+    email: email,
+    password: password,
   });
+
+  if (userExists) {
+    const token = jwt.sign(
+      {
+        id: userExists._id,
+      },
+      JWT_USER_PASSWORD,
+    );
+    res.status(200).json({
+      token: token,
+    });
+  } else {
+    res.status(403).json({
+      message: "Invalid credentials",
+    });
+  }
 });
 
 userRouter.get("/purchases", (req, res) => {
